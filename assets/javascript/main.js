853c15e97f69235c9aae8ad75a3f3a2981bf5324
Luis Suarez <suarezluis@gmail.com>
2022-01-29 15:43:56 -0600

    dynamic year

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,11 +4,14 @@ const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
+const year = document.getElementById("year");
 
 // Set Initial State Of Menu
 let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
+
+year.innerHTML = new Date().getFullYear();
 
 function toggleMenu() {
   if (!showMenu) {
@@ -16,7 +19,7 @@ function toggleMenu() {
     menu.classList.add("show");
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
-    navItems.forEach(item => item.classList.add("show"));
+    navItems.forEach((item) => item.classList.add("show"));
 
     // Set Menu State
     showMenu = true;
@@ -25,7 +28,7 @@ function toggleMenu() {
     menu.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
-    navItems.forEach(item => item.classList.remove("show"));
+    navItems.forEach((item) => item.classList.remove("show"));
 
     // Set Menu State
     showMenu = false;
